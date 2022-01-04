894513d272a295d277dc085ce123aabffaf0dab8
Florian Morello <arsonik@me.com>
2022-01-04 09:11:02 -0500

    do not override sequelize declaration

--- packages/sequelize-redis-cache/src/index.ts
+++ packages/sequelize-redis-cache/src/index.ts
@@ -1,12 +1,11 @@
-import { Model, QueryOptionsWithModel } from 'sequelize';
 
 export * from './dbFactoryCache';
 export * from './sequelizeCache';
 export * from './interfaces';
 
 // todo waiting merge on https://github.com/sequelize/sequelize/pull/12585
-declare module 'sequelize' {
-    interface Sequelize {
-        query<M extends Model>(sql: string | { query: string; values: unknown[] }, options: QueryOptionsWithModel<M> & { plain: true }): Promise<M | null>;
-    }
-}
+// declare module 'sequelize' {
+//     interface Sequelize {
+//         query<M extends Model>(sql: string | { query: string; values: unknown[] }, options: QueryOptionsWithModel<M> & { plain: true }): Promise<M | null>;
+//     }
+// }
