3da64a01d33e762d5cbc0db6c4f375976de77fb3
Florian Morello <arsonik@me.com>
2023-01-27 10:58:28 +0100

    models

--- packages/sequelize-dataloader/src/multiple.ts
+++ packages/sequelize-dataloader/src/multiple.ts
@@ -7,43 +7,48 @@ import { RedisDataLoader, RedisDataloaderOptions } from '@daryl-software/redis-d
 import { SequelizeModelDataloaderOptions } from './index';
 import { ModelNotFoundError } from '@daryl-software/error';
 
+// given User = { id: number, email: string, other: string }
+// MultipleDataloader(User, ['id', 'email'], options) - loader.load({ id: 1, email: 'xxx' })
+// MultipleDataloader(User, 'id', options) - loader.load({ id: 1, email: 'xxx' })
 export function MultipleDataloader<
-    V extends Model,
-    K extends keyof V,
-    Key extends K | K[],
-    KeyToLoad extends Pick<V, K> | V[K], // can be a single key (id) or a combination of keys [id, email]
-    Options extends SequelizeModelDataloaderOptions<KeyToLoad, V, string, V[]> & Partial<RedisDataloaderOptions<KeyToLoad, V>>,
-    BatchFn = (keys: readonly KeyToLoad[]) => KeyToLoad extends V[K] ? typeof BatchLoader<V, K, 'filter'> : typeof BatchLoaderMultiColumns<V, K, 'filter'>,
-    Return = Options extends RedisDataloaderOptions<KeyToLoad, V> ? RedisDataLoader<KeyToLoad, V[], string> : DataLoader<KeyToLoad, V[], string>
->(model: ModelStatic<V>, key: Key, options?: Options): Return {
-    let batchLoadFn: BatchFn;
+    M extends Model,
+    MKey extends keyof M,
+    KeyOrKeys extends MKey | MKey[], // [id, email] or id
+    KeyToLoad extends KeyOrKeys extends MKey[] ? Pick<M, MKey> : M[MKey], // { id: 1, email: 'xxx' } or 1
+    MinOptions extends SequelizeModelDataloaderOptions<KeyToLoad, M, string, M[]>,
+    WithRedisOptions extends MinOptions & RedisDataloaderOptions<KeyToLoad, M>,
+    Options extends MinOptions | WithRedisOptions,
+    TBatchLoader = KeyOrKeys extends MKey ? typeof BatchLoader<M, MKey, 'filter'> : typeof BatchLoaderMultiColumns<M, MKey, 'filter'>,
+    RedisReturn = RedisDataLoader<KeyToLoad, M[], string>,
+    RegularReturn = DataLoader<KeyToLoad, M[], string>,
+    Return = Options extends WithRedisOptions ? RedisReturn : RegularReturn
+>(model: ModelStatic<M>, key: KeyOrKeys, options?: Options): Return {
+    let batchLoadFn: (keys: readonly KeyToLoad[]) => TBatchLoader;
     let cacheKeyFn: (keyToLoad: KeyToLoad) => string;
 
     if (Array.isArray(key)) {
-        // @ts-ignore
-        batchLoadFn = (keys: readonly Pick<V, K>[]) => BatchLoaderMultiColumns(model, key, keys, 'filter', options);
-        cacheKeyFn = (ak) => key.map((o) => (ak as Pick<V, K>)[o]).join(',');
+        batchLoadFn = ((keys: readonly Pick<M, MKey>[]) => BatchLoaderMultiColumns(model, key, keys, 'filter', options)) as any;
+        cacheKeyFn = (ak) => key.map((o) => (ak as Pick<M, MKey>)[o]).join(',');
     } else {
-        // @ts-ignore
-        batchLoadFn = (keys: readonly V[K][]) => BatchLoader(model, key, keys, 'filter', options);
+        batchLoadFn = ((keys: readonly M[MKey][]) => BatchLoader(model, key as MKey, keys, 'filter', options)) as any;
         cacheKeyFn = (ak) => JSON.stringify(ak);
     }
     if (options && 'redis' in options) {
         // @ts-ignore
-        return new RedisDataLoader<K, V[]>(`${model.name}-${key.toString()}-MULTI`, batchLoadFn, {
-            // @ts-ignore
+        return new RedisDataLoader<MKey, M[]>(`${model.name}-${key.toString()}-MULTI`, batchLoadFn, {
             cacheKeyFn,
             notFound: (akey) => new ModelNotFoundError(model, akey),
             maxBatchSize: 100,
             ...options,
             redis: {
                 ...options.redis,
-                deserialize: (_, json) => (JSON.parse(json) as CreationAttributes<V>[]).map((entry) => hydrateModel(model, entry)),
+                deserialize: (_, json) => (JSON.parse(json) as CreationAttributes<M>[]).map((entry) => hydrateModel(model, entry)),
                 serialize: (data) => JSON.stringify(data.map((o) => o.get({ plain: true }))),
             },
-        }) as unknown as Return;
+        }) as any;
     } else {
-        return new DataLoader<KeyToLoad, V[], string>(
+        // @ts-ignore
+        return new DataLoader<KeyToLoad, M[], string>(
             (keys) =>
                 // @ts-ignore
                 batchLoadFn(keys).then((values) =>
@@ -55,6 +60,6 @@ export function MultipleDataloader<
                     })
                 ),
             { cacheKeyFn, ...options }
-        ) as Return;
+        );
     }
 }
